e73983d2ad17ea140d6ee15a46ea3f3fba37e7cc
Sree <sreelekshmi.geetha@neueda.com>
2021-07-30 12:36:51 +0100

    Added the findByName

--- src/main/java/com/neueda/javablockingmicroservicechassis/controller/ChassisController.java
+++ src/main/java/com/neueda/javablockingmicroservicechassis/controller/ChassisController.java
@@ -2,19 +2,27 @@ package com.neueda.javablockingmicroservicechassis.controller;
 
 import com.neueda.javablockingmicroservicechassis.entity.ChassisEntity;
 import com.neueda.javablockingmicroservicechassis.service.ChassisService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1")
 public class ChassisController {
 
-    @Autowired
-    private ChassisService chassisService;
+    private final ChassisService chassisService;
+
+    public ChassisController(ChassisService chassisService) {
+        this.chassisService = chassisService;
+    }
 
     @GetMapping("/chassis")
     public ResponseEntity<?> getAllChassis(){
--- src/main/java/com/neueda/javablockingmicroservicechassis/service/ChassisService.java
+++ src/main/java/com/neueda/javablockingmicroservicechassis/service/ChassisService.java
@@ -2,19 +2,17 @@ package com.neueda.javablockingmicroservicechassis.service;
 
 import com.neueda.javablockingmicroservicechassis.entity.ChassisEntity;
 import com.neueda.javablockingmicroservicechassis.repository.ChassisRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
-import static org.springframework.data.domain.ExampleMatcher.matching;
 
 @Service
 public class ChassisService {
 
-    @Autowired
-    private ChassisRepository chassisRepository;
+    private final ChassisRepository chassisRepository;
+
+    public ChassisService(ChassisRepository chassisRepository) {
+        this.chassisRepository = chassisRepository;
+    }
 
     public List<ChassisEntity> retrieveAllChassis(){
         return chassisRepository.findAll();
@@ -25,12 +23,7 @@ public class ChassisService {
     }
 
     public List<ChassisEntity> searchChassisByName(String name){
-        ChassisEntity chassisEntity = new ChassisEntity();
-        chassisEntity.setName(name);
-        ExampleMatcher matcher = matching().withMatcher("name", contains());
-        Example<ChassisEntity> example = Example.of(chassisEntity, matcher);
-
-        return chassisRepository.findAll(example);
+        return chassisRepository.findByName(name);
 
     }
 
