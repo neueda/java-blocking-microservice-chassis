4f3d47fa437b4abf6e7b317a192b99ab375cdc69
Sree <sreelekshmi.geetha@neueda.com>
2021-07-30 12:56:18 +0100

    Added the findByName

--- src/main/java/com/neueda/javablockingmicroservicechassis/service/ChassisService.java
+++ src/main/java/com/neueda/javablockingmicroservicechassis/service/ChassisService.java
@@ -3,17 +3,12 @@ package com.neueda.javablockingmicroservicechassis.service;
 import com.neueda.javablockingmicroservicechassis.entity.ChassisEntity;
 import com.neueda.javablockingmicroservicechassis.repository.ChassisRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import java.util.List;
-import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
-import static org.springframework.data.domain.ExampleMatcher.matching;
 
 @RequiredArgsConstructor
 @Service
 public class ChassisService {
-
 
     private final ChassisRepository chassisRepository;
 
@@ -26,20 +21,13 @@ public class ChassisService {
     }
 
     public List<ChassisEntity> searchChassisByName(String name){
-        ChassisEntity chassisEntity = new ChassisEntity();
-        chassisEntity.setName(name);
-        ExampleMatcher matcher = matching().withMatcher("name", contains());
-        Example<ChassisEntity> example = Example.of(chassisEntity, matcher);
-
-        return chassisRepository.findAll(example);
-
+        return chassisRepository.findByName(name);
     }
 
     public ChassisEntity addChassis(ChassisEntity chassis){
         ChassisEntity chassisEntity = new ChassisEntity();
         chassisEntity.setName(chassis.getName());
         chassisEntity.setDescription(chassis.getDescription());
-
         return chassisRepository.save(chassisEntity);
     }
 
