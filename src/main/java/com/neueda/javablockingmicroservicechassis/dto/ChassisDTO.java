8756fafc3b46587b11aec2daa8f9205ec040b374
Sree <sreelekshmi.geetha@neueda.com>
2021-07-30 13:33:08 +0100

    Added the DTO

---
+++ src/main/java/com/neueda/javablockingmicroservicechassis/dto/ChassisDTO.java
@@ -0,0 +1,14 @@
+package com.neueda.javablockingmicroservicechassis.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChassisDTO {
+
+    private String name;
+    private String description;
+}
