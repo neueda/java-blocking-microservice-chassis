d62c84601c896eb7f6798b5ea1cdfe5ab31b3d2e
Berchris Requiao <berchris.requiao@neueda.com>
2021-08-04 10:42:55 +0100

    Refactor testGetAllChassis

--- src/test/java/com/neueda/javablockingmicroservicechassis/controller/ChassisControllerTest.java
+++ src/test/java/com/neueda/javablockingmicroservicechassis/controller/ChassisControllerTest.java
@@ -7,6 +7,7 @@ import com.neueda.javablockingmicroservicechassis.entity.ChassisEntity;
 import com.neueda.javablockingmicroservicechassis.repository.ChassisRepository;
 import com.neueda.javablockingmicroservicechassis.service.ChassisService;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,6 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -37,18 +39,22 @@ public class ChassisControllerTest {
 
     @Test
     public void testGetAllChassis() throws Exception {
-        List<ChassisEntity> chassisEntityList = new ArrayList<>();
-        chassisEntityList.add(new ChassisEntity(1L,"name 1","description 1"));
-        chassisEntityList.add(new ChassisEntity(2L,"name 2","description 2"));
-        Mockito.when(chassisService.retrieveAllChassis()).thenReturn(chassisEntityList);
-        String url = "/v1/chassis";
-        mockMvc.perform(get(url)).andExpect(status().isOk());
+        // given
+        when(chassisService.retrieveAllChassis())
+                .thenReturn(List.of(
+                        new ChassisEntity(1L,"name 1","description 1"),
+                        new ChassisEntity(2L,"name 2","description 2")));
+
+        // when
+        mockMvc.perform(get("/v1/chassis"))
+                // then
+                .andExpect(status().isOk());
     }
 
     @Test
     public void testGetChassisById() throws Exception {
         ChassisEntity chassisEntity = new ChassisEntity(5L,"name 5","description 5");
-        Mockito.when(chassisService.retriveChassisById(5L)).thenReturn(chassisEntity);
+        when(chassisService.retriveChassisById(5L)).thenReturn(chassisEntity);
         String url = "/v1/chassis/5";
         mockMvc.perform(get(url)).andExpect(status().isOk());
     }
@@ -59,7 +65,7 @@ public class ChassisControllerTest {
         chassisEntityList.add(new ChassisEntity(1L,"name","description 1"));
         chassisEntityList.add(new ChassisEntity(2L,"name","description 2"));
         String name = "name";
-        Mockito.when(chassisService.searchChassisByName(name)).thenReturn(chassisEntityList);
+        when(chassisService.searchChassisByName(name)).thenReturn(chassisEntityList);
         String url = "/v1/chassisSearch/name";
         mockMvc.perform(get(url)).andExpect(status().isOk());
     }
@@ -68,7 +74,7 @@ public class ChassisControllerTest {
     public void testCreate() throws Exception {
         ChassisEntity chassisEntity = new ChassisEntity(6L,"name 6","description 6");
         ChassisDTO chassisDTO = new ChassisDTO("name 6","description 6");
-        Mockito.when(chassisService.addChassis(chassisDTO)).thenReturn(chassisEntity);
+        when(chassisService.addChassis(chassisDTO)).thenReturn(chassisEntity);
         String url = "/v1/chassis";
         mockMvc.perform(post(url).contentType("application/json").content(objectMapper.writeValueAsString(chassisDTO))).andExpect(status().isCreated());
     }
