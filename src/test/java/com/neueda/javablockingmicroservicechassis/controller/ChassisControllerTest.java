6636e5f970cc0ce573c1e72893565734f9609d4a
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-04 16:52:00 +0100

    Add testDeleteChassis()

--- src/test/java/com/neueda/javablockingmicroservicechassis/controller/ChassisControllerTest.java
+++ src/test/java/com/neueda/javablockingmicroservicechassis/controller/ChassisControllerTest.java
@@ -13,13 +13,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ChassisController.class)
@@ -44,7 +45,6 @@ class ChassisControllerTest {
                 .thenReturn(List.of(
                         new ChassisEntity(1L,"name 1","description 1"),
                         new ChassisEntity(2L,"name 2","description 2")));
-
         // when
         mockMvc.perform(get("/v1/chassis"))
                 // then
@@ -65,12 +65,12 @@ class ChassisControllerTest {
     void testGetChassisById() throws Exception {
         //given
         when(chassisService.retriveChassisById(5L)).thenReturn(new ChassisEntity(5L,"name 5","description 5"));
-
         //when
         mockMvc.perform(get("/v1/chassis/5"))
                 //then
                 .andExpect(status().isOk());
     }
+
     @Test
     void testGetChassisById_EntityNotFound() throws Exception {
         //given
@@ -108,27 +108,38 @@ class ChassisControllerTest {
     @Test
     void testCreate() throws Exception {
         //given
-        ChassisEntity chassisEntity = new ChassisEntity(6L,"name 6","description 6");
         ChassisDTO chassisDTO = new ChassisDTO("name 6","description 6");
-        when(chassisService.addChassis(chassisDTO)).thenReturn(chassisEntity);
+        when(chassisService.addChassis(new ChassisDTO("name 6","description 6"))).thenReturn( new ChassisEntity(6L,"name 6","description 6"));
         //when
-        mockMvc.perform(post("/v1/chassis").contentType("application/json").content(objectMapper.writeValueAsString(chassisDTO)))
+        mockMvc.perform(post("/v1/chassis")
+                        .contentType("application/json")
+                        .content(objectMapper.writeValueAsString(chassisDTO)))
                 //then
                 .andExpect(status().isCreated());
     }
 
     @Test
-    void testDeleteChassis() {
+    void testDeleteChassis() throws Exception {
+        //given
+        ChassisEntity chassisEntity = new ChassisEntity(6L, "name 6", "description 6");
+        doNothing().when(chassisService).deleteChassis(chassisEntity.getId());
+        //when
+        mockMvc.perform(delete("/v1/chassis/6").contentType(MediaType.APPLICATION_JSON))
+                //then
+                .andExpect(status().isNoContent());
+    }
+
+
         // given
         // ToDo: This is just a exception test example it have to be replaced by a real method test
-        when(chassisService.retrieveAllChassis())
-                .thenThrow(IllegalArgumentException.class);
+        //when(chassisService.retrieveAllChassis())
+         //       .thenThrow(IllegalArgumentException.class);
 
         // when
-        ThrowingCallable methodUnderTest = () -> chassisService.retrieveAllChassis();
+        //ThrowingCallable methodUnderTest = () -> chassisService.retrieveAllChassis();
 
         // then
-        BDDAssertions.thenThrownBy(methodUnderTest)
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        //BDDAssertions.thenThrownBy(methodUnderTest)
+          //      .isInstanceOf(IllegalArgumentException.class);
+
 }
