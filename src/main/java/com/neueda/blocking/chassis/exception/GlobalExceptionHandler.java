7d94e2de800603b547cf174c9938b6692b3388a8
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-09 16:48:18 +0100

    modify GlobalExceptionHandler

--- src/main/java/com/neueda/blocking/chassis/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neueda/blocking/chassis/exception/GlobalExceptionHandler.java
@@ -1,19 +1,28 @@
 package com.neueda.blocking.chassis.exception;
 
+import com.neueda.blocking.chassis.model.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
-    @Value(value = "${data.exception.message1}")
-    private String message1;
 
-    @ExceptionHandler(value = ChassisEntityNotFoundException.class)
-    public ResponseEntity ChassisEntityNotFoundException(ChassisEntityNotFoundException chassisEntityNotFoundException) {
-        return new ResponseEntity<String>(message1, HttpStatus.NOT_FOUND);
+    @ExceptionHandler(ChassisEntityNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private ErrorResponse handleChassisEntityNotFoundException(ChassisEntityNotFoundException ex) {
+        return logAndRespond(ex, ex.getPath());
+    }
+    private ErrorResponse logAndRespond(Exception ex, String path) {
+        String errorMsg = ex.getLocalizedMessage();
+        log.error(errorMsg, ex);
+
+        return new ErrorResponse(errorMsg, path, ex.getClass().getTypeName());
     }
 
 }
