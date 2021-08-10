2f91157c118645281f4557fdf906229d0b737a32
Pushvinder Singh Nanda <pushvindersingh.nanda@gmail.com>
2021-08-10 11:13:48 +0100

    Changes to the few files. And converting Chassis.java to Record!

--- src/main/java/com/neueda/blocking/chassis/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neueda/blocking/chassis/exception/GlobalExceptionHandler.java
@@ -2,9 +2,7 @@ package com.neueda.blocking.chassis.exception;
 
 import com.neueda.blocking.chassis.model.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
