dbdb7e968a02e85d37b19f94892d65e6f30eb6c9
Pushvinder Singh Nanda <pushvindersingh.nanda@gmail.com>
2021-08-11 09:44:12 +0100

    Added Comments to Controller Test

--- src/test/java/com/neueda/blocking/chassis/controller/ChassisControllerTests.java
+++ src/test/java/com/neueda/blocking/chassis/controller/ChassisControllerTests.java
@@ -41,6 +41,7 @@ class ChassisControllerTests {
     @Test
     @DisplayName("testing get method to get all values")
     void testGetAllChassis() throws Exception {
+        //Given
         final String name = RandomStringUtils.randomAlphabetic(5);
         final String description1 = RandomStringUtils.randomAlphabetic(6);
         final String description2 = RandomStringUtils.randomAlphabetic(7);
@@ -52,32 +53,36 @@ class ChassisControllerTests {
         final Chassis chassis2 = new Chassis(name, description2);
         final List<Chassis> expected = List.of(chassis1, chassis2);
 
+        //when
         mockMvc.perform(get("/v1/chassis"))
-
+                //then
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(expected)));
     }
 
     @Test
     void testGetAllChassis_EntityNotFound() throws Exception {
-
+        //Given
         when(chassisService.retrieveAllChassis()).thenReturn(null);
 
+        //when
         mockMvc.perform(get("/v1/chassis"))
-
+                //then
                 .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Testing get values by Id method")
     void testGetChassisById() throws Exception {
-
+        //Given
         final String name = RandomStringUtils.randomAlphabetic(5);
         final String description = RandomStringUtils.randomAlphabetic(6);
         when(chassisService.retrieveChassisById(5L)).thenReturn(new ChassisEntity(5L,name,description));
         final Chassis expected = new Chassis(name, description);
 
+        //when
         mockMvc.perform(get("/v1/chassis/5"))
+                //then
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(expected)));
     }
@@ -85,14 +90,19 @@ class ChassisControllerTests {
     @Test
     @DisplayName("Testing get value by id not found method")
     void testGetChassisById_EntityNotFound() throws Exception {
+        //Given
         when(chassisService.retrieveChassisById(5L)).thenReturn(null);
+
+        //when
         mockMvc.perform(get("/v1/chassis/5"))
+                //then
                 .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Testing get values by name method")
     void testGetChassisByName() throws Exception {
+        //Given
         final String name = RandomStringUtils.randomAlphabetic(5);
         final String description1 = RandomStringUtils.randomAlphabetic(6);
         final String description2 = RandomStringUtils.randomAlphabetic(7);
@@ -106,8 +116,10 @@ class ChassisControllerTests {
         final Chassis chassis1 = new Chassis(name, description1);
         final Chassis chassis2 = new Chassis(name, description2);
         final List<Chassis> expected = List.of(chassis1, chassis2);
-        mockMvc.perform(get("/v1/chassisSearch/" + name))
 
+        //when
+        mockMvc.perform(get("/v1/chassisSearch/" + name))
+                //then
                 .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(expected)));
     }
@@ -115,31 +127,41 @@ class ChassisControllerTests {
     @Test
     @DisplayName("Testing get by name not found ")
     void testGetChassisByName_EntityNotFound() throws Exception {
-
+        //Given
         String name = "name";
         when(chassisService.searchChassisByName(name)).thenReturn(null);
+
+        //when
         mockMvc.perform(get("/v1/chassisSearch/name"))
+                //then
                 .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("Testing create method")
     void testCreate() throws Exception {
-
+        //Given
         Chassis chassis = new Chassis("name 6","description 6");
         when(chassisService.addChassis(new Chassis("name 6","description 6"))).thenReturn( new ChassisEntity(6L,"name 6","description 6"));
+
+        //when
         mockMvc.perform(post("/v1/chassis")
+                        //then
                         .contentType("application/json")
                         .content(objectMapper.writeValueAsString(chassis)))
-                .andExpect(status().isCreated());
+                        .andExpect(status().isCreated());
     }
 
     @Test
     @DisplayName("Testing Delete method")
     void testDeleteChassis() throws Exception {
+        //Given
         ChassisEntity chassisEntity = new ChassisEntity(6L, "name 6", "description 6");
         doNothing().when(chassisService).deleteChassis(chassisEntity.getId());
+
+        //when
         mockMvc.perform(delete("/v1/chassis/6").contentType(MediaType.APPLICATION_JSON))
+                //then
                 .andExpect(status().isOk());
     }
 
