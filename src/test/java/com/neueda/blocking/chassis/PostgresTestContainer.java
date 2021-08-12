fd02d517ac73d4e3975c16cdd71afc3b199e95ab
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-12 09:10:01 +0100

    add PostgresTestContainer

---
+++ src/test/java/com/neueda/blocking/chassis/PostgresTestContainer.java
@@ -0,0 +1,27 @@
+package com.neueda.blocking.chassis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
+
+@Slf4j
+public class PostgresTestContainer {
+
+    private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:13.3-alpine");
+
+    static {
+        postgresContainer.start();
+        postgresContainer.followOutput(new Slf4jLogConsumer(log).withSeparateOutputStreams());
+    }
+
+    @DynamicPropertySource
+    private static void setDatasourceProperties(DynamicPropertyRegistry registry) {
+
+        registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgresContainer::getUsername);
+        registry.add("spring.datasource.password", postgresContainer::getPassword);
+    }
+}
