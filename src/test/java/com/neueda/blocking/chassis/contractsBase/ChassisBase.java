d5f5a0db7b5631690c54eb6f811f149d7f605fcd
Farhat-Ghani <farhat.ghani@neueda.com>
2021-08-19 12:16:34 +0100

    Contract test updated.

--- src/test/java/com/neueda/blocking/chassis/contractsBase/ChassisBase.java
+++ src/test/java/com/neueda/blocking/chassis/contractsBase/ChassisBase.java
@@ -1,40 +1,44 @@
 package com.neueda.blocking.chassis.contractsBase;
 
+import com.neueda.blocking.chassis.client.GithubClient;
+import com.neueda.blocking.chassis.configuration.RestDocsConfig;
 import com.neueda.blocking.chassis.controller.ChassisController;
 import com.neueda.blocking.chassis.entity.ChassisEntity;
 import com.neueda.blocking.chassis.service.ChassisService;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.List;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
+@Tag("ContractTest")
 @WebMvcTest(ChassisController.class)
-public class ChassisBase extends BaseContractTest {
-    @Autowired
-    private ChassisController chassisController;
+@AutoConfigureRestDocs
+@Import(RestDocsConfig.class)
+public abstract class ChassisBase {
 
     @MockBean
     private ChassisService chassisService;
 
-    @Autowired
-    private MockMvc mockMvc;
+    @MockBean
+    private GithubClient githubClient;
 
     @BeforeEach
-    void setUp() {
-
+    void setUp(@Autowired MockMvc mockMvc) {
+        //init
         RestAssuredMockMvc.mockMvc(mockMvc);
 
+        //given
         List<ChassisEntity> response = List.of(
                 new ChassisEntity(1L,"test name","test description"));
-        when(chassisService.retrieveAllChassis())
-                .thenReturn(response);
-
+        given(chassisService.retrieveAllChassis())
+                .willReturn(response);
     }
-
 }
+
