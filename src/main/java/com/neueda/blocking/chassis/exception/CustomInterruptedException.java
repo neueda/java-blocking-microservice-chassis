0d3e1ec20381863088fd742c83482b0046848d13
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-23 16:03:44 +0100

    Add CustomInterruptedException

---
+++ src/main/java/com/neueda/blocking/chassis/exception/CustomInterruptedException.java
@@ -0,0 +1,8 @@
+package com.neueda.blocking.chassis.exception;
+
+
+public class CustomInterruptedException extends Exception {
+    public CustomInterruptedException(String message) {
+        super(message);
+    }
+}
