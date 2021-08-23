aaac8ef42eff0c706922f976b947db38a91f91a4
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-23 16:05:23 +0100

    Add CustomIOexception

---
+++ src/main/java/com/neueda/blocking/chassis/exception/CustomIOexception.java
@@ -0,0 +1,7 @@
+package com.neueda.blocking.chassis.exception;
+
+public class CustomIOexception extends RuntimeException{
+    public CustomIOexception(String message) {
+        super(message);
+    }
+}
