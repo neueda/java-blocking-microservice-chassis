e95e458b49d8d63cc6a8226d8a39dc83030d6958
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-24 13:44:20 +0100

    Modify GithubClient

--- src/main/java/com/neueda/blocking/chassis/client/GithubClient.java
+++ src/main/java/com/neueda/blocking/chassis/client/GithubClient.java
@@ -2,6 +2,7 @@ package com.neueda.blocking.chassis.client;
 
 import java.net.http.HttpResponse;
 
+import com.neueda.blocking.chassis.exception.CustomException;
 import com.neueda.blocking.chassis.properties.ClientProperties;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Service;
@@ -15,13 +16,13 @@ public class GithubClient {
         this.clientHelper = new ClientHelper(clientProps);
     }
 
-    public String searchUsernameContaining(@NonNull String value) {
+    public String searchUsernameContaining(@NonNull String value) throws CustomException {
 
-        HttpResponse<String> response = clientHelper.performGetRequest(uriBuilder -> uriBuilder
+        HttpResponse<?> response = clientHelper.performGetRequest(uriBuilder -> uriBuilder
                 .pathSegment("search")
                 .pathSegment("users")
                 .queryParam("q", value.concat("+repos:>0"))
                 .build());
-        return response.body();
+        return response.body().toString();
     }
 }
