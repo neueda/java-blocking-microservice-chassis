2f17c7aa68361a0bd019186e6b795a668dacb551
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-24 14:00:42 +0100

    change ControllerAdvice to RestControllerAdvice

--- src/main/java/com/neueda/blocking/chassis/exception/GlobalExceptionHandler.java
+++ src/main/java/com/neueda/blocking/chassis/exception/GlobalExceptionHandler.java
@@ -3,11 +3,11 @@ package com.neueda.blocking.chassis.exception;
 import com.neueda.blocking.chassis.model.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
