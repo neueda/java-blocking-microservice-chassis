1e1963b59af591c8db081788fcf1c8d7126128a1
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-25 10:28:00 +0100

    update CustomException

--- src/main/java/com/neueda/blocking/chassis/exception/CustomException.java
+++ src/main/java/com/neueda/blocking/chassis/exception/CustomException.java
@@ -2,9 +2,15 @@ package com.neueda.blocking.chassis.exception;
 
 public class CustomException extends Exception {
 
+    private final String path;
 
-    public CustomException(String message) {
+    public CustomException(String message, Exception e, String path) {
         super(message);
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
     }
 
 }
