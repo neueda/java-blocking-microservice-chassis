0c2c16ecfabc941755966ec72055f24e726475d2
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-08-26 15:50:31 +0100

    merge from master

--- src/main/java/com/neueda/blocking/chassis/model/ErrorResponse.java
+++ src/main/java/com/neueda/blocking/chassis/model/ErrorResponse.java
@@ -2,11 +2,13 @@ package com.neueda.blocking.chassis.model;
 
 import static java.util.Objects.requireNonNull;
 
-public record ErrorResponse(String error, String path, String exception) {
+public record ErrorResponse(Integer statusCode, String error, String description, String path) {
 
     public ErrorResponse {
+        requireNonNull(statusCode);
         requireNonNull(error);
+        requireNonNull(description);
         requireNonNull(path);
-        requireNonNull(exception);
     }
+
 }
