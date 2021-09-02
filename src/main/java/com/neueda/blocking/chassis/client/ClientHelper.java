6f55d17f753cef2950f606ca771f4467e2dbd0e2
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-09-02 11:54:56 +0100

    Modified ClientHelper

--- src/main/java/com/neueda/blocking/chassis/client/ClientHelper.java
+++ src/main/java/com/neueda/blocking/chassis/client/ClientHelper.java
@@ -1,10 +1,13 @@
 package com.neueda.blocking.chassis.client;
 
-import com.neueda.blocking.chassis.exception.CustomException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.neueda.blocking.chassis.properties.ClientProperties;
 
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.util.UriBuilder;
+import static org.springframework.web.util.UriComponentsBuilder.fromUri;
 
 import java.io.IOException;
 import java.net.URI;
@@ -13,36 +16,35 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.function.Function;
 
-import static com.neueda.blocking.chassis.constants.ChassisConstants.CLIENT_URL;
-import static org.springframework.web.util.UriComponentsBuilder.fromUri;
-
 @Slf4j
 public class ClientHelper {
 
     private final HttpClient httpClient;
     private final URI baseUrl;
+    private final ObjectMapper objectMapper;
 
     ClientHelper(ClientProperties clientProps) {
         this.httpClient = HttpClient.newHttpClient();
         this.baseUrl = clientProps.baseUrl();
+        this.objectMapper = new ObjectMapper();
     }
 
-    <T> T performGetRequest(Function<UriBuilder, URI> uriFunction) {
-        T res = null;
+    <T> T performGetRequest(Function<UriBuilder, URI> uriFunction, Class<T> clazz) {
+
         try {
             HttpRequest request = HttpRequest.newBuilder()
                     .GET()
                     .header("accept", "application/json")
                     .uri(uriFunction.apply(fromUri(baseUrl)))
                     .build();
-            res =  (T) httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+            String response = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
+            return clazz.isAssignableFrom(String.class) ? (T) response : objectMapper.readValue(response, clazz);
         }
         catch (IOException | InterruptedException e) {
-            log.error("this thread is interrupted or i/o error", e);
-            throw new CustomException(e.getMessage(), CLIENT_URL);
+            log.error("Failed to send GET request", e);
+            throw new RestClientException(e.getMessage(), e);
         }
-
-        return res;
     }
+
 }
