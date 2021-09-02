8c2ec6ea4662e45b81f6e7cb63f969b2525b8b4c
Reshmi Sajeesh <reshmi.pm1980@gmail.com>
2021-09-02 10:49:05 +0100

    remove unused exception

--- src/main/java/com/neueda/blocking/chassis/exception/CustomIOexception.java
+++
@@ -1,7 +0,0 @@
-package com.neueda.blocking.chassis.exception;
-
-public class CustomIOexception extends RuntimeException{
-    public CustomIOexception(String message) {
-        super(message);
-    }
-}
